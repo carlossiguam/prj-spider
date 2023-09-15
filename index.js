7b519ecb43ab67c000b8ba87d8e2e158a2b95542
carlos sigua <carlos.sigua@gmail.com>
2023-09-15 03:29:21 -0500

    example open web page emov mail

---
+++ index.js
@@ -0,0 +1,14 @@
+import puppeteer from "puppeteer";
+
+async function openWebPage(){
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 200
+    })
+    const page = await browser.newPage()
+
+    await page.goto('https://mail.emov.gob.ec')
+    await browser.close()
+}
+
+openWebPage();
